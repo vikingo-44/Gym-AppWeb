76105ec58ee200d9e2eb2f0a4fd856798c210958
vikingo-44 <gonza.federico44@gmail.com>
2025-12-24 19:28:53 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Agregamos base: './' para que las rutas de los assets sean relativas
+  // Esto evita el error "Not Found" al desplegar en subcarpetas como GitHub Pages
+  base: './',
   build: {
     outDir: 'dist',
   }
